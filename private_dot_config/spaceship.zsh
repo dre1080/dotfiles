7f85de13e15e1480b79758508a1e12f27e81ec8b
ando <lifeandcoding@gmail.com>
2023-12-21 00:52:17 +0300

    update spaceship config

--- private_dot_config/spaceship.zsh
+++ private_dot_config/spaceship.zsh
@@ -13,19 +13,20 @@ SPACESHIP_PROMPT_ORDER=(
   php            # PHP section
   rust           # Rust section
   erlang         # Erlang section
+  exec_time      # Execution time
   line_sep       # Line break
   sudo           # Sudo indicator
   char           # Prompt character
 )
 SPACESHIP_RPROMPT_ORDER=(
+  exit_code      # Exit code section
   time           # Time stamps section
-  exec_time      # Execution time
   async          # Async jobs indicator
   jobs           # Background jobs indicator
-  exit_code      # Exit code section
 )
 SPACESHIP_TIME_SHOW=true
 SPACESHIP_USER_SHOW=always
 SPACESHIP_DIR_TRUNC_REPO=false
 SPACESHIP_PROMPT_PREFIXES_SHOW=true
 SPACESHIP_PROMPT_SUFFIXES_SHOW=true
+SPACESHIP_DIR_TRUNC=0
