35536b5b9891d19ac50f4dc70c6dfe6c381abb74
ando <lifeandcoding@gmail.com>
2024-06-11 12:55:44 +0300

    Add `sheldon` as new shell plugin manager
    * This replaces `zgenom`. It's much faster & convenient to work with.
    
    See: https://sheldon.cli.rs

---
+++ dot_zsh/aliases.zsh
@@ -0,0 +1,21 @@
+# Directories
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# Tools
+alias g='git'
+alias p='podman'
+alias docker='podman'
+alias yt='yt-dlp'
+alias m='micro'
+alias x='mise x --'
+alias cz=chezmoi
+alias erlv="erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), \"releases\", erlang:system_info(otp_release), \"OTP_VERSION\"])), io:fwrite(Version), halt().' -noshell"
+alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+
+# Git
+alias gcm='git commit -m'
+alias gcam='git commit -a -m'
+alias gcad='git commit -a --amend'
