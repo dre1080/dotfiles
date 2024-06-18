ba794960f0b6b5702ad47052b450edbd81694368
dre1080 <lifeandcoding@gmail.com>
2024-06-18 03:34:18 +0300

    Fix order of script execution

--- .chezmoiscripts/run_onchange_before_02_install-mise.sh
+++ .chezmoiscripts/run_onchange_before_02_install-mise.sh
@@ -4,12 +4,12 @@ set -e
 
 [[ -x ~/.local/bin/mise ]] || curl mise.run | sh
 
-# needed for elixir phoenix framework
-sudo apt install -y inotify-tools
-
 # Install default programming languages
 languages=(erlang elixir bun sqlite node go php rust)
 
 for language in $languages; do
  ~/.local/bin/mise use -g $language@latest
 done
+
+# needed for elixir phoenix framework
+sudo apt install -y inotify-tools
